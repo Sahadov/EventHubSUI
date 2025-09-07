7df1b305c3a625aa78e6a5e407599977da90c72b
Dmitry Volkov <dmitrijvolkov@MacBook-Air-Dmitry.local>
2025-09-07 18:33:54 -0300

    Skeleton project

---
+++ EventHubSUI/EventHubSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventHubSUIApp.swift
+//  EventHubSUI
+//
+//  Created by Dmitry Volkov on 07/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct EventHubSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
