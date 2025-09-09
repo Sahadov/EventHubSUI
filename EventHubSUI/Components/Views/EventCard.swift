aef0e43731e809dcd9229f83132c0d745c3ff5b2
Dmitry Volkov <dmitrijvolkov@MacBook-Air-Dmitry.local>
2025-09-08 22:08:27 -0300

    Add reusable event card

--- EventHubSUI/Components/Views/EventCard.swift
+++ EventHubSUI/Components/Views/EventCard.swift
@@ -18,29 +18,43 @@ struct EventCard: View {
     var event: Event
     
     var body: some View {
-        HStack(spacing: 15) {
+        HStack(alignment: .top, spacing: 15) {
             ImageLoaderView()
                 .aspectRatio(0.8, contentMode: .fit)
                 .clipShape(RoundedRectangle(cornerRadius: 10))
             VStack(alignment: .leading) {
-                Text("Wed, Apr 8 5:30 PM")
-                    .font(.Airbnb.book(size: 17))
-                    .foregroundStyle(.accentBlue)
+                HStack {
+                    Text("Wed, Apr 8 5:30 PM")
+                        .font(.Airbnb.book(size: 17))
+                        .foregroundStyle(.accentBlue)
+                    Spacer()
+                    if type == .favourites {
+                        Image(systemName: "bookmark.fill")
+                            .foregroundStyle(Color.accentRed)
+                    }
+                }
                 Spacer()
                 Text(event.title)
                     .font(.Airbnb.medium(size: 18))
+                    .lineLimit(2)
+                    .truncationMode(.tail)
+                    .multilineTextAlignment(.leading)
                 Spacer()
-                HStack {
-                    Image("map-pin")
-                        .resizable()
-                        .frame(width: 15, height: 15)
-                    Text("Lot 13 Oakland, CA")
-                        .font(.Airbnb.book(size: 17))
-                        .foregroundStyle(.secondary)
+                if type == .events || type == .favourites  {
+                    HStack {
+                        Image("map-pin")
+                            .resizable()
+                            .frame(width: 15, height: 15)
+                        Text("Lot 13 Oakland, CA")
+                            .font(.Airbnb.book(size: 17))
+                            .foregroundStyle(.secondary)
+                    }
                 }
             }
+            
         }
         .padding(10)
+        .frame(maxWidth: .infinity, minHeight: 106, maxHeight: 106, alignment: .leading)
         .background(Color.white)
         .clipShape(RoundedRectangle(cornerRadius: 10))
     }
@@ -49,7 +63,12 @@ struct EventCard: View {
 #Preview {
     ZStack {
         Color.gray.ignoresSafeArea()
-        EventCard(event: Event.mockConcert)
-            .frame(width: 900, height: 106)
+        VStack(spacing: 16) {
+            EventCard(event: Event.mockConcert)
+            EventCard(type: .search, event: Event.mockConcert)
+            EventCard(type: .favourites, event: Event.mockConcert)
+        }
+        .padding()
+        .frame(maxWidth: 330)
     }
 }
