1fe54a2f8443d089d800a5598823e3e216c73823
abayellow <sasha.abanshin@gmail.com>
2025-09-12 13:39:14 +1100

    Add callback for FilterScrollView

--- EventHubSUI/Core/Explore/Views/FilterScrollView.swift
+++ EventHubSUI/Core/Explore/Views/FilterScrollView.swift
@@ -14,16 +14,24 @@ enum FilterCategory: String, CaseIterable {
 }
 
 struct FilterScrollView: View {
+    var onButtonTap: (() -> Void)?
+    
     var body: some View {
         HStack(spacing: 12) {
             ForEach(FilterCategory.allCases, id: \.self) { filter in
-                Text(filter.rawValue)
-                    .font(.system(size: 14, weight: .bold))
-                    .foregroundColor(.white)
-                    .frame(width: 106.77, height: 39.06)
-                    .background(Color(hex: "#4A43EC"))
-                    .cornerRadius(20.96)
-                    .shadow(radius: 2, y: 1)
+                Button {
+                    onButtonTap?()
+                    print("Category taped: \(filter.rawValue)") // for test
+                } label: {
+                    Text(filter.rawValue)
+                        .font(.system(size: 14, weight: .bold))
+                        .foregroundColor(.white)
+                        .frame(width: 106.77, height: 39.06)
+                        .background(Color(hex: "#4A43EC"))
+                        .cornerRadius(20.96)
+                        .shadow(radius: 2, y: 1)
+                }
+                
             }
         }
         .padding(.horizontal)
