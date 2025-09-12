ca2dd7806f6160340e1887620941bad6364533de
abayellow <sasha.abanshin@gmail.com>
2025-09-12 13:43:06 +1100

    Edit ExploreImageView

--- EventHubSUI/Core/Explore/Views/ExploreCell/ExploreImageView.swift
+++ EventHubSUI/Core/Explore/Views/ExploreCell/ExploreImageView.swift
@@ -8,36 +8,62 @@
 import SwiftUI
 
 struct ExploreImageView: View {
+    let event: Event
     let screenWidth = UIScreen.main.bounds.width
-
+    
     var body: some View {
-        Image(.mockEvent)
-            .resizable()
-            .scaledToFill()
-            .frame(width: screenWidth * 0.7, height: screenWidth * 0.7 * 0.72)
-            .clipped()
-            .cornerRadius(screenWidth * 0.03)
-            .overlay(
-                HStack {
-                    dateView
-                        .padding(.leading, screenWidth * 0.02)
-                    Spacer()
-                    bookmarkView
-                        .padding(.trailing, screenWidth * 0.02)
-                }
-                .padding(.top, screenWidth * 0.02),
-                alignment: .top
-            )
+//        ImageLoaderView(
+//            urlString: event.images?.first?.thumbnails?.size144x96 ?? "https://picsum.photos/640/384?2",
+//            resizingMode: .fill
+//        )
+//        Image(.mockEvent)
+//        .resizable()
+//        .scaledToFill()
+        AsyncImage(url: URL(string: event.images?.first?.thumbnails?.size144x96 ?? "https://images.unsplash.com/photo-1507874457470-272b3c8d8ee2?w=144")) { phase in
+            switch phase {
+            case .empty:
+                ProgressView() // Пока загружается
+            case .success(let image):
+                image
+                    .resizable()
+                    .scaledToFill()
+            case .failure(_):
+                Image(.mockEvent) // Фолбэк при ошибке
+                    .resizable()
+                    .scaledToFill()
+            @unknown default:
+                EmptyView()
+            }
+        }
+        .frame(width: screenWidth * 0.7, height: screenWidth * 0.7 * 0.72)
+        .clipped()
+        .cornerRadius(screenWidth * 0.03)
+        .overlay(
+            HStack {
+                dateView
+                    .padding(.leading, screenWidth * 0.02)
+                Spacer()
+                bookmarkView
+                    .padding(.trailing, screenWidth * 0.02)
+            }
+            .padding(.top, screenWidth * 0.02),
+            alignment: .top
+        )
     }
+
+
 
     var dateView: some View {
         VStack(spacing: screenWidth * 0.01) {
-            Text("11")
-                .font(.system(size: screenWidth * 0.05, weight: .bold))
-            Text("June")
-                .font(.system(size: screenWidth * 0.035))
+            Text(event.dates?.first?.day ?? "")
+                .font(.system(size: screenWidth * 0.06, weight: .thin))
+            
+                
+            Text(event.dates?.first?.month ?? "")
+                .font(.system(size: screenWidth * 0.026, weight: .regular))
+                
         }
-        .foregroundColor(.black)
+        .foregroundColor(.red)
         .frame(width: screenWidth * 0.15, height: screenWidth * 0.15)
         .background(
             RoundedRectangle(cornerRadius: screenWidth * 0.02)
@@ -66,5 +92,6 @@ struct ExploreImageView: View {
 
 
 #Preview {
-    ExploreImageView()
+    ExploreImageView(event: .mockConcert)
+        
 }
