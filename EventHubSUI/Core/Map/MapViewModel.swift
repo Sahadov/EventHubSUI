f25c9b8c0a05398f96ba046797acb898b735c5a7
Dmitry Volkov <dmitrijvolkov@MacBook-Air-Dmitry.local>
2025-09-13 21:33:25 -0300

    Refactor MapView

--- EventHubSUI/Core/Map/MapViewModel.swift
+++ EventHubSUI/Core/Map/MapViewModel.swift
@@ -6,10 +6,19 @@
 //
 
 import Foundation
+import MapKit
+import CoreLocation
 
 @MainActor
 class MapViewModel: ObservableObject {
     private var networkService = NetworkService()
+    @Published var searchText: String = ""
+    @Published var currentLocation: CLLocationCoordinate2D?
+    @Published var mapRegion = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 55.7569, longitude: 37.6151),
+        span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+    )
+    @Published var tappedEvent: Event?
     
     @Published var upcomingEvents: [Event] = []
     
