8f9035d7067d5736034b2307a85e6d96fa329145
abayellow <sasha.abanshin@gmail.com>
2025-09-19 17:51:35 +1100

    Modify TabBar on GOOD LOOKING

--- EventHubSUI/Core/TabBar/TabBarButton.swift
+++ EventHubSUI/Core/TabBar/TabBarButton.swift
@@ -5,30 +5,54 @@
 //  Created by Sergey on 08.09.2025.
 //
 
-import Foundation
+
 import SwiftUI
 
 struct TabButton: View {
-        
     let tab: TabBookmarksEnum
     let isSelected: Bool
     let action: () -> Void
     
     var body: some View {
         Button(action: action) {
-            Image(isSelected ? tab.iconSelected : tab.icon)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 40, height: 40)
-//                .foregroundColor(.black)
-//                .background {
-//                    if isSelected {
-//                        Color.blue.opacity(0.2)
-//                            .clipShape(RoundedRectangle(cornerRadius: 10))
-//                            .frame(width: 22, height: 22)
-//                    }
-//                }
-            
+            VStack(spacing: 4) {
+                Image(systemName: tab.icon)
+                    .font(.system(size: 20, weight: .regular))
+                    .foregroundColor(isSelected ? .blue : .gray)
+                
+                Text(tab.title)
+                    .font(.caption2)
+                    .foregroundColor(isSelected ? .blue : .gray)
+            }
+            .frame(maxWidth: .infinity)
         }
     }
 }
+
+//import Foundation
+//import SwiftUI
+//
+//struct TabButton: View {
+//        
+//    let tab: TabBookmarksEnum
+//    let isSelected: Bool
+//    let action: () -> Void
+//    
+//    var body: some View {
+//        Button(action: action) {
+//            Image(isSelected ? tab.iconSelected : tab.icon)
+//                .resizable()
+//                .aspectRatio(contentMode: .fit)
+//                .frame(width: 40, height: 40)
+////                .foregroundColor(.black)
+////                .background {
+////                    if isSelected {
+////                        Color.blue.opacity(0.2)
+////                            .clipShape(RoundedRectangle(cornerRadius: 10))
+////                            .frame(width: 22, height: 22)
+////                    }
+////                }
+//            
+//        }
+//    }
+//}
