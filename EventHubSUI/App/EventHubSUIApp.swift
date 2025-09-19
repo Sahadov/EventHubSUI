7cc4d60335c70293255f373e3dd0f5301691040e
Dmitry Volkov <dmitrijvolkov@MacBook-Air-Dmitry.local>
2025-09-18 21:01:02 -0300

    Add bypass to SignIn

--- EventHubSUI/App/EventHubSUIApp.swift
+++ EventHubSUI/App/EventHubSUIApp.swift
@@ -55,7 +55,7 @@ struct EventHubSUIApp: App {
                         case .eventDetailScreen(let event):
                             EventDetailsView(event: event)
                         case .resetPasswordConfirmationScreen:
-                            ResetViewConfirm()
+                            ResetViewConfirm(resetVM: ResetViewModel(validator: validator, router: router))
                         case .seeAllScreen(events: let events, isLoading: let loading):
                             SeeAllContentView(events: events, isLoading: loading)
                         case .listScreen(events: let events):
