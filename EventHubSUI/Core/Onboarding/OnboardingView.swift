765b599c6c47bef718c76ee938013bbb0bc1652f
Dmitry Volkov <dmitrijvolkov@MacBook-Air-Dmitry.local>
2025-09-21 23:30:11 -0300

    Add animation

--- EventHubSUI/Core/Onboarding/OnboardingView.swift
+++ EventHubSUI/Core/Onboarding/OnboardingView.swift
@@ -78,11 +78,13 @@ struct OnboardingView: View {
                                     Spacer()
                                     
                                     Button(action: {
-                                        if currentPage < onboardingData.count - 1 {
-                                            currentPage += 1
-                                        } else {
-                                            OnboardingManager.completeOnboarding()
-                                            shouldShowOnboarding = false
+                                        withAnimation(.easeInOut(duration: 1)) {
+                                            if currentPage < onboardingData.count - 1 {
+                                                currentPage += 1
+                                            } else {
+                                                OnboardingManager.completeOnboarding()
+                                                shouldShowOnboarding = false
+                                            }
                                         }
                                     }) {
                                         Text("Next")
